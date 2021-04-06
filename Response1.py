c339205d70c29c3bf4e46f75a37e5d9e3525e44a
Professor68 <81517451+Professor68@users.noreply.github.com>
2021-04-06 01:21:49 -0500

    Add files via upload

---
+++ Response1.py
@@ -0,0 +1,9 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/TRANSVISION OP MNGR/Documents/PythonInstructions/First_code.py
+Enter first and last name Malvern Maturure
+ Malvern Maturure
+
+What are your goals for this class - long term goals?
+What would you like to be....when all is said and done with the class? To be better acquainted with python and to stretch my limits and imagination as far as this program permits me. To learn and use innovative, productive and helpful uses of python and be able to grow myself and others.
