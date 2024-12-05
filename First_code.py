5bc7bfe3ccc70d5025a9bb23f5c7f8e9b59ea29b
Professor68 <81517451+Professor68@users.noreply.github.com>
2024-12-04 21:48:49 -0600

    I made some changes and changes to this code. There are challenges in the code related to the README file. Let's Go
    exit

--- First_code.py
+++ First_code.py
@@ -1,8 +1,32 @@
 #Enter first and last name
 names = input ("Enter first and last name")
-print(names)
-print()
+#Update the print function to return a string response.
+print(f"The user's names are {names}")
 #Enter your goals for this class
-print("What are your goals for this class - long term goals?")
+# Need to create a method a method that asks the question
+
+def question():
+    question = input("Do you have goals? Y/N"\n)
+    
+    
+    if question = 'y': #Ask for question
+        goals1 = input("What are your goals for this class - long term goals?")
+        
+    else:
+        print(f'click next')
+        
+question()
+    
+
 goals = input('What would you like to be....when all is said and done with the class?')
-print(goals)
+print(f"{goals}")
+print(f'Thank you, {first_name}, for taking this short survey.\n A mentor will be assigned to you after you at the end of your orientation. Good Luck!')
+
+#Write a seperate code that will be called into this one that will assign new students ID numbers according to how they join the team and assigns them a temporary random 15 character password with a high complexity according to current security standards.
+
+#Offer the user to play a game at the end of their enrollment. They should be able to choose from at list 3 games. Basket ball, Black Jack or Rock Paper scissors. All matches will be against the computer.
+
+#If they win offer to send an ecrypted message on what gift they would like once they complete their coursework successfully and a bonus after completing that task successfully
+
+#Grade the student's work and determine their success from their teachers point of view.
+#Let's Go!
